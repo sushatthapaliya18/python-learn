dd9d67d503d67dba0d49d426382b0cd877513f57
Sushant Thapaliya <sushantthapaliya7@gmail.com>
2025-06-25 15:07:30 +0545

    'html css

--- practise/day_02.py
+++ practise/day_02.py
@@ -3,3 +3,30 @@ a= 'hello'
 b= 'world'
 c= a+ ' '+b
 print(c)
+
+# fstring
+name="sushant"
+age=22
+print(f'my name is {name}.my age is {age}')
+
+v=2
+r=3
+if r>v:
+    print("r is greater than v")
+
+else:
+    print("v is greater than r")
+
+i=1
+if i<6:
+    print(i)
+    if i==3:
+        break
+        i+=1:
+
+
+
+
+
+
+
