3903105b40dccb373227448a9f83249eeb4b6289
Sushant Thapaliya <sushantthapaliya7@gmail.com>
2025-06-26 11:56:44 +0545

    factorial

---
+++ practise/day_03.py
@@ -0,0 +1,13 @@
+# check odd even 
+def is_even(n):
+    return "Even" if n % 2 == 0 else "Odd"
+
+    # factorial calculator
+    def factorial(n):
+        result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+    
+
+
